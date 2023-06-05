1d8832e8f54dee5f0cb91fa97be1c4d24a88376b
Mayankkumar821 <102612687+Mayankkumar821@users.noreply.github.com>
2023-06-05 17:42:10 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+  {
+    name: "Akhiyaan",
+    artist: "Mitraz",
+    img: "music1",
+    src: "music1"
+  },
+  {
+    name: "Gul",
+    artist: "Anuv Jain",
+    img: "music2",
+    src: "music2"
+  },
+  {
+    name: "Khoya",
+    artist: "Zariya",
+    img: "music3",
+    src: "music3"
+  },
+  {
+    name: "Kali Kali Zulfon Ke",
+    artist: "Madur Sharma",
+    img: "music4",
+    src: "music4"
+  },
+  {
+    name: "Dooriyan",
+    artist: "Dino James",
+    img: "music5",
+    src: "music5"
+  },
+  {
+    name: "Wishlist",
+    artist: "Dino James",
+    img: "music6",
+    src: "music6"
+  },
+  {
+    name: "Rangisari",
+    artist: "unknown",
+    img: "music7",
+    src: "music7"
+  },
+  {
+    name: "Chalo Chalein",
+    artist: "Ritviz",
+    img: "music8",
+    src: "music8"
+  },
+];
